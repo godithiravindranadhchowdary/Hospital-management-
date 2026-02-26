92180e9a822cba48dad610bf9f9e3f5ea3387ae8
pavankumar2266 <pavankumarkotaru2000@gmail.com>
2026-02-26 14:28:24 +0530

    Fix ALLOWED_HOSTS for Render deployment

--- push.bat
+++ push.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d c:\Users\Pavan Kumar\Desktop\hospital management
 git add -A
-git commit -m "Add Vercel config for deployment"
+git commit -m "Fix ALLOWED_HOSTS for Render deployment"
 git push origin main
 pause
